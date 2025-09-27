cbdf4958e2de0a5d61f0a59fd0826402ee7dcb89
Sami Khayatei Houssaini <sami_houssaini@berkeley.edu>
2025-09-26 22:16:14 -0700

    procedural updates

---
+++ server/graphqlschema.py
@@ -0,0 +1,7 @@
+# Create our GraphQL schemas here
+# If they become too hectic, we will create standalone files for each subgroup
+# Should all contain the basic imports as defined here
+
+import strawberry
+import typing
+
---
+++ server/server.py
@@ -0,0 +1,5 @@
+# Execute this file to run the server
+
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
