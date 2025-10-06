d2ee41349af809dd70287b9dae2ddd4f69c319cc
Jayden Szeto <jayden2007@berkeley.edu>
2025-10-05 17:01:48 -0700

    added user registration. supbase_service is for creating user and also seeing if the user exists. user_routes is to test the reigigration and also see if it works. server i added router.

--- server/server.py
+++ server/server.py
@@ -1,5 +1,11 @@
-# Execute this file to run the server
-
 from fastapi import FastAPI
-from strawberry.fastapi import GraphQLRouter
+from user_routes import router as user_router
 
+app = FastAPI(title="Prompt Party API")
+
+app.include_router(user_router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
